4980b8b9ec63b3dbdc3de3be20a8889b5080a158
Nikita Shub <nikitushu2@gmail.com>
2025-04-02 13:22:11 +0300

    initialize heroku

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,8 +6,15 @@ import { PORT } from "./util/config";
 import { connectToDatabase } from "./util/db";
 import { router as itemsRouter } from "./controllers/items";
 import { errorHandler, AppError } from "./middleware/errorHandler";
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production' 
+    ? 'https://illusia-project-2947763ee03d.herokuapp.com' 
+    : 'http://localhost:5173'
+}));
 
 app.use(express.json());
 
